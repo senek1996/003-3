87a2f1ec45211af379969902815dce47fe361f01
unknown <bai1541b@k3-426-10.edu.ugrasu>
2016-02-22 08:55:42 +0500

    789

---
+++ src/лаб/работа/pkg3/pkg3/ЛабРабота33.java
@@ -0,0 +1,21 @@
+/*
+ * Эта прога выводит массив из 15 чисел от 0 до 9 и считает кол-во четных эл-ов.
+   Автор: Бредихин А.И.
+ */
+package лаб.работа.pkg3.pkg3;
+
+public class ЛабРабота33 {
+
+    public static void main(String[] args) {
+        int i,n=15, kolvo=0;
+        int[] a;
+        a=new int[16];
+        for (i=1; i<=n; i++) {
+            a[i]=(int)(Math.random()*10);
+            if (a[i]%2==0) {kolvo++;}
+            System.out.print(a[i]+" ");
+        }
+        System.out.print("\nЧетных чисел: "+kolvo);
+    }
+    
+}
